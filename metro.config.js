c5c02de37473a5ffe74bcf634679c57fe15e77b0
axelPortatil <axelperez221105@gmail.com>
2025-08-31 20:56:24 +0200

    rebuilding android

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,17 @@
-const { getDefaultConfig } = require("expo/metro-config");
+// metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Add resolver for metro-cache FileStore
+config.resolver.extraNodeModules = {
+    ...config.resolver.extraNodeModules,
+    './src/stores/FileStore': require.resolve('metro-cache/src/stores/FileStore')
+};
+
+// Also add resolver for TerminalReporter if needed
+config.resolver.extraNodeModules['./src/lib/TerminalReporter'] =
+    require.resolve('metro/src/lib/TerminalReporter');
 
 module.exports = config;
